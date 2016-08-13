4a5951b702ca4e77d292c20c9fb1125bf542c76e
litian <tify5236@qq.com>
2016-08-13 16:35:28 +0800

    初始添加

---
+++ app/src/main/java/com/phone1000/android_program/MainActivity.java
@@ -0,0 +1,18 @@
+package com.phone1000.android_program;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    /**
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
